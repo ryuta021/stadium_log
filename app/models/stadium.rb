66c0d315a20cba879be2dd00dbb67853ff509c1b
ryuta <ryuta046@gmail.com>
2019-11-14 02:04:04 +0000

    コミット内容の確認

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -7,12 +7,12 @@ class Stadium < ApplicationRecord
 
 
 
- private
-  def geocode
-    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=AIzaSyBXVVA0A41o5mRG_hSO9KrzOp73q5ES91c")
-    res = HTTP.get(uri).to_s
-    response = JSON.parse(res)
-    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
-  end
+#  private
+#   def geocode
+#     uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=AIzaSyDgRNuMMQ26SbHwrGQmhxfrS4kjcuygGbI")
+#     res = HTTP.get(uri).to_s
+#     response = JSON.parse(res)
+#     self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+#     self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+#   end
 end
