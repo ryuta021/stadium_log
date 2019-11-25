5887a358ee812ca38cedbc28bb63883895e0a497
ryuta <ryuta046@gmail.com>
2019-11-25 16:51:13 +0000

    migrationファイルの修正

--- db/migrate/20191118003349_create_spots.rb
+++ db/migrate/20191118003349_create_spots.rb
@@ -1,3 +1,4 @@
+
 class CreateSpots < ActiveRecord::Migration[5.2]
   def change
     create_table :spots do |t|
--- db/migrate/20191118033252_add_column_plans.rb
+++ db/migrate/20191118033252_add_column_plans.rb
@@ -1,6 +1,6 @@
 class AddColumnPlans < ActiveRecord::Migration[5.2]
   def change
   	add_column :plans, :title, :string
-  	add_column :spots,  :spot_title, :string
+
   end
 end
